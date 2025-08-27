954e99f8a78cd2b6461a9bf5a249eb205a1b61ce
suhyunnnnnni <dastar998176@gmail.com>
2025-08-28 02:07:03 +0900

    loginView 조건 추가

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -7,7 +7,8 @@
             </div>
 
             <!-- id&password -->
-            <div id="container" class="d-flex flex-column justify-content-center align-items-center gap-3 w-75 h-30 p-5">
+            <div id="container"
+                class="d-flex flex-column justify-content-center align-items-center gap-3 w-75 h-30 p-5">
 
                 <div class="d-flex justify-content-center align-items-center w-100 h-25">
 
@@ -15,9 +16,8 @@
                         <span class="path1"></span>
                         <span class="path2"></span>
                     </i>
-                    <input type="text" placeholder="아이디" 
-                            class="form-control bg-transparent border border-transparent text-white fs-2" 
-                            v-model="idInput">
+                    <input type="text" placeholder="아이디"
+                        class="form-control bg-transparent border border-transparent text-white fs-2" v-model="idInput">
 
                 </div>
 
@@ -31,13 +31,13 @@
                         <span class="path3"></span>
                     </i>
                     <input type="password" placeholder="비밀번호"
-                        class="form-control bg-transparent border border-transparent text-white fs-2"
-                        v-model="pwInput">
+                        class="form-control bg-transparent border border-transparent text-white fs-2" v-model="pwInput">
 
                 </div>
 
                 <div class="d-flex justify-content-center w-100 h-25">
-                    <button class="btn btn-primary d-flex flex-fill justify-content-center align-items-center mx-2" @click="goToSleep()">로그인</button>
+                    <button class="btn btn-primary d-flex flex-fill justify-content-center align-items-center mx-2"
+                        @click="goToSleep()">로그인</button>
                 </div>
 
             </div>
@@ -45,6 +45,64 @@
             <!-- 회원 가입 -->
             <div class="d-flex justify-content-center align-items-start mt-10 w-100 h-25">
                 <button class="btn fs-2 fw-bold text-white " @click="goToSignIn()">회원 가입</button>
+            </div>
+
+            <!--begin::Alert-->
+            <div class="alert alert-dismissible bg-light-danger border border-danger d-flex flex-column flex-sm-row p-5 mb-10"
+                v-if="showAlert">
+
+                <i class="ki-duotone ki-notification fs-2hx text-danger me-4 mb-5 mb-sm-0">
+                    <span class="path1"></span>
+                    <span class="path2"></span>
+                    <span class="path3"></span>
+                </i>
+
+                <!--begin::Wrapper-->
+                <div class="d-flex flex-column pe-0 pe-sm-10">
+
+                    <h5 class="mb-1">{{ status }}</h5>
+                    <span class="contents">{{ contents }}</span>
+
+                </div>
+                <!--end::Wrapper-->
+
+                <!--begin::Close-->
+                <button type="button"
+                    class="position-absolute position-sm-relative m-2 m-sm-0 top-0 end-0 btn btn-icon ms-sm-auto"
+                    data-bs-dismiss="alert">
+                    <i class="ki-duotone ki-cross fs-1 text-danger"><span class="path1"></span><span
+                            class="path2"></span></i>
+                </button>
+                <!--end::Close-->
+            </div>
+            <!--end::Alert-->
+
+            <!-- modal -->
+            <div class="modal fade" tabindex="-1" id="kt_modal_1">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h3 class="modal-title">성공!</h3>
+
+                            <!--begin::Close-->
+                            <div class="btn btn-icon btn-sm btn-active-light-primary ms-2" data-bs-dismiss="modal"
+                                aria-label="Close">
+                                <i class="ki-duotone ki-cross fs-1"><span class="path1"></span><span
+                                        class="path2"></span></i>
+                            </div>
+                            <!--end::Close-->
+                        </div>
+
+                        <div class="modal-body">
+                            <p>성공적으로 회원가입이 이루어졌습니다.</p>
+                        </div>
+
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-primary" data-bs-dismiss="modal"
+                                @click="goToLogin()">로그인하러 가기</button>
+                        </div>
+                    </div>
+                </div>
             </div>
 
         </div>
@@ -56,9 +114,77 @@
 import { useRouter } from 'vue-router';
 const router = useRouter()
 
+import { ref, onMounted } from 'vue'
+const idInput = ref('')
+const pwInput = ref('')
+
+// alert
+const status = ref('')
+const contents = ref('')
+const showAlert = ref(false);
+
+// modal
+let modalInstance = null
+
+const output = ref('')
+
 function goToSleep() {
-    router.push('/sleep')
+
+    if (idInput.value && pwInput.value) {
+
+        const username = idInput.value
+        const password = pwInput.value
+
+        const user = {
+
+            username: username,
+            password: password,
+
+        }
+
+        requestCheck(user)
+        if (output.value == '200') {
+            modalInstance.show()
+        } else {
+            status.value = '알림'
+            contents.value = '아이디와 비밀번호가 일치하지 않습니다. '
+            showAlert.value = true;
+        }
+
+
+
+
+    } else {
+
+        status.value = '경고'
+        contents.value = '아이디와 비밀번호 모두 입력하세요'
+        showAlert.value = true;
+    }
+    // router.push('/sleep')
 }
+
+// id,pw 일치 확인
+async function requestCheck(user) {
+
+    try {
+        const response = await axios({
+            method: 'post',
+            baseURL: "http://localhost:8001/",
+            url: '/user/v1/add',
+            data: user,
+            timeout: 5000,
+            responseType: 'json'
+        })
+
+        console.log(`응답 -> ${JSON.stringify(response.data)}`)
+
+        output.value = response.data.code
+
+    } catch (err) {
+        console.error(`에러 -> ${err}`)
+    }
+}
+
 
 function goToSignIn() {
     router.push('/sign')
@@ -77,7 +203,7 @@ function goToSignIn() {
 }
 
 .title {
-    text-shadow: 2px 2px 5px rgba(255,255,255,0.5);
+    text-shadow: 2px 2px 5px rgba(255, 255, 255, 0.5);
 }
 
 #container {
@@ -89,11 +215,8 @@ function goToSignIn() {
     background: linear-gradient(360deg, #5d93fe, #6578f7)
 }
 
-.h-30{
+.h-30 {
     height: 30% !important;
 }
 
-.no-hover {
-    background-color: transparent;
-}
 </style>
